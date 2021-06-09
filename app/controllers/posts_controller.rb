80d5902faed92a9ccb73ce0482c525c5e4d58ec1
emi ota <ametio.145@gmail.com>
2021-06-09 02:46:37 +0000

    createメソッドを作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,13 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+  def create
+    Post.create(post_params)
+    redirect_to new_post_path
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:name, :content)
+  end
 end
