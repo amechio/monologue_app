af3d49ae367eb1248d70a3b8df7549bd8c49aa8a
emi ota <ametio.145@gmail.com>
2021-06-09 13:53:17 +0000

    _form.html.erbを作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update]
   def index
     @posts = Post.all
   end
@@ -17,15 +18,15 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
+    #@post = Post.find(params[:id])
   end
 
   def edit
-    @post = Post.find(params[:id])
+    #@post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
+    #@post = Post.find(params[:id])
     if @post.update(post_params)
       redirect_to posts_path, notice: "ツイートを編集しました！"
     else
@@ -36,5 +37,9 @@ class PostsController < ApplicationController
   private
   def post_params
     params.require(:post).permit(:name, :content)
+  end
+
+  def set_blog
+    @post = Post.find(params[:id])
   end
 end
