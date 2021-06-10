3bb6ef6d81b5cbb5455e77d4bf27d6a8e1286186
emi ota <ametio.145@gmail.com>
2021-06-10 05:16:36 +0000

    endを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   #get '/posts', to: 'posts#index'
   #get '/stocks', to: 'stocks#index'
   resources :posts do
-  collection do
+    collection do
       post :confirm
     end
+  end
 end
