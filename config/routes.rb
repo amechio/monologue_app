b5f9e2011e7a2beb62eb253398d4dc1e042f47cc
emi ota <ametio.145@gmail.com>
2021-06-10 04:50:04 +0000

    投稿確認画面を作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get '/posts', to: 'posts#index'
-  get '/stocks', to: 'stocks#index'
-  resources :posts
+  #get '/posts', to: 'posts#index'
+  #get '/stocks', to: 'stocks#index'
+  resources :posts do
+  collection do
+      post :confirm
+    end
 end
